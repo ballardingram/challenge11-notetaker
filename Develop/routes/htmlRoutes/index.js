997f9435acdf6f6fef17637da6e7a7d413ac1f97
Ballard Ingram <ballardingram@outlook.com>
2022-05-06 09:35:25 -0500

    added files and started routes

---
+++ Develop/routes/htmlRoutes/index.js
@@ -0,0 +1,6 @@
+// DOCUMENTATION > API ROUTES WITH EXPRESS (https://expressjs.com/en/api.html)
+// ROUTES > EXTERNAL ROUTES
+const path = require("path");
+const router = require('express').Router();
+
+module.exports = router;
