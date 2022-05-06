f446199e22c462ed52a62527f0a000ec20cba4bc
Ballard Ingram <ballardingram@outlook.com>
2022-05-06 10:18:18 -0500

    index.js - installed uuid and updated functions import, restructured db.json for array

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,6 +1,13 @@
 // DOCUMENTATION > API ROUTES WITH EXPRESS (https://expressjs.com/en/api.html)
+// DOCUMENTATION > UUID (https://www.npmjs.com/package/uuid)
+// NOTE > UUID - SYNTAX PULLED DIRECTLY FROM SITE TO SUE WITH COMMONJS
 // ROUTES > EXTERNAL ROUTES
 const router = require('express').Router();
+const { v4: uuidv4} = require('uuid');
+
+//IMPORT > FUNCTIONS REQUIRED FROM LIB/NOTE.JS
+const {updateDB, newNote} = require("../../lib/note");
+const {note} = require("../../db/db.json")
 
 
 module.exports = router;
